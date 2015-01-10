6a0c5db9ad0745c680e5225520278651a8fc47f6
Kasper Lewau <lewauu@gmail.com>
2015-01-10 13:18:49 +0100

    Add lowGraphs addon

---
+++ lowGraphs/lowGraphs.lua
@@ -0,0 +1,44 @@
+-- CPU
+SetCVar('m2Faster', 1);
+
+-- GFX
+SetCVar('ffx', 0);
+SetCVar('hwPCF', 1);
+SetCVar('shadowlod', 0);
+SetCVar('timingmethod', 1);
+SetCVar('showshadow', 0);
+SetCVar('showfootprints', 0);
+SetCVar('showfootprintparticles', 0);
+
+SetCVar('overridefarclip', 0);
+SetCVar('farclip', 177);
+SetCVar('horizonfarclip', 1305);
+SetCVar('detailDoodadAlpha', 0);
+SetCVar('groundeffectdensity', 16);
+
+SetCVar('groundeffectdist', 1);
+SetCVar('smallcull', 1);
+SetCVar('skycloudlod', 1);
+SetCVar('characterAmbient', 1);
+SetCVar('extshadowquality', 0);
+SetCVar('environmentDetail', 0.5);
+
+-- /console m2Faster 1
+-- /console ffx 0
+-- /console hwPCF 1
+-- /console shadowlod 0
+-- /console timingmethod 1
+-- /console showshadow 0
+-- /console showfootprints 0
+-- /console showfootprintparticles 0
+-- /console overridefarclip 0
+-- /console farclip 177
+-- /console horizonfarclip 1305
+-- /console detailDoodadAlpha 0
+-- /console groundeffectdensity 16
+-- /console groundeffectdist 1
+-- /console smallcull 1
+-- /console skycloudlod 1
+-- /console characterAmbient 1
+-- /console extshadowquality 0
+-- /console environmentDetail 0.5
